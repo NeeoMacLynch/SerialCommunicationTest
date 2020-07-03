eff16fe7521f1b51d3155bab0d861d8d280e5b75
Lee <qqdblue9533@live.com>
2020-07-03 17:32:38 +0800

    add new function

--- app/src/main/java/com/example/serialcommunicationtest/util/DataConversionUtils.java
+++ app/src/main/java/com/example/serialcommunicationtest/util/DataConversionUtils.java
@@ -1,9 +1,5 @@
 package com.example.serialcommunicationtest.util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class DataConversionUtils {
     private static String baseHex =  "0123456789ABCDEF";
 
@@ -68,5 +64,72 @@ public class DataConversionUtils {
         };
     }
 
+    /**
+     * 把十六进制表示的字节数组字符串，转换成十六进制字节数组
+     *
+     * @param hex -目标十六进制字符串
+     * @return byte[]
+     */
+    public static byte[] hexStr2bytes(String hex) {
+        int len = (hex.length() / 2);
+        byte[] result = new byte[len];
+        char[] chars = hex.toUpperCase().toCharArray();
+        for (int i = 0; i < len; i++) {
+            int pos = i * 2;
+            result[i] = (byte) (hexChar2byte(chars[pos]) << 4 | hexChar2byte(chars[pos + 1]));
+        }
+        return result;
+    }
+
+    /**
+     * 把16进制字符[0123456789abcde]（含大小写）转成字节
+     *
+     * @param c -目标字符
+     * @return 对应子节
+     */
+    private static int hexChar2byte(char c) {
+        switch (c) {
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            case 'a':
+            case 'A':
+                return 10;
+            case 'b':
+            case 'B':
+                return 11;
+            case 'c':
+            case 'C':
+                return 12;
+            case 'd':
+            case 'D':
+                return 13;
+            case 'e':
+            case 'E':
+                return 14;
+            case 'f':
+            case 'F':
+                return 15;
+            default:
+                return -1;
+        }
+    }
 
 }
